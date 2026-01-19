b80b0158b5962f0f47a28f360c7f1816899912df
Ankit-kumar8-1 <ankitksoftware@gmail.com>
2026-01-19 18:05:23 +0530

    chore: add core dependencies and application configuration - Added core Spring Boot dependencies (Web, Security, JPA, Validation, Lombok) - Integrated JWT and Spring Mail dependencies - Configured database connection properties - Added mail service configuration - Enabled multipart support for image and video uploads - Defined media upload directories and server limits

---
+++ src/main/java/in/ankit_Saahariya/stream_verse/StreamVerseApplication.java
@@ -0,0 +1,13 @@
+package in.ankit_Saahariya.stream_verse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamVerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamVerseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/ankit_Saahariya/stream_verse/StreamVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ankit_Saahariya.stream_verse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamVerseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
